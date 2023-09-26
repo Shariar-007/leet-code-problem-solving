1dacf8608fcae447cd006c8639f3b4dc5a3e6c80
Shariar Shohag <mohammad.al-shariar@gim.com.bd>
2023-09-26 18:39:55 -0400

    palinrome problem solved

---
+++ Palindrome_Number.java
@@ -0,0 +1,23 @@
+package Leet_code_problem_solving;
+
+import java.util.Scanner;
+
+public class Palindrome_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		System.out.println(isPlindrome(number));
+	}
+	
+	static boolean isPlindrome(int number) {
+		for(int i=0; i<String.valueOf(number).length()/2; i++) {
+			if(String.valueOf(number) != null && String.valueOf(number).charAt(i) != String.valueOf(number).charAt(String.valueOf(number).length() -1 - i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
